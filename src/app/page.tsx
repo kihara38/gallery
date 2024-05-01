f8f00bce4b4d57ed55f116be5329cc64f5fc1fd0
kihara38 <85784773+kihara38@users.noreply.github.com>
2024-05-01 10:34:19 +0300

    url images and sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
+import Gallery from "./components/gallery"
 
 export default function Home() {
-  return  <h1>hello world</h1>
+  return  <Gallery/>
 }
---
+++ src/lib/env.ts
@@ -0,0 +1,7 @@
+import { CleanedEnv, cleanEnv, str } from "envalid";
+
+const env= cleanEnv(process.env,{
+    PEXELS_API_KEY:str(),
+})
+
+export default env
